ae9af08693c650f3ff3ca8824f7b7e2169012867
Veodm <odoev2014@mail.ru>
2023-04-26 14:47:05 +0300

    Рефакторинг

--- FormMain.cs
+++ FormMain.cs
@@ -23,71 +23,69 @@ namespace Trading_results
     public partial class formMain : Form
     {
 
-        static int positionRowTo = 9;
-        static FileStream ReadMaket = new FileStream("res\\maket.xlsx", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
-        static XLWorkbook bookTo = new XLWorkbook(ReadMaket);
-        static IXLWorksheet sheetTo = bookTo.Worksheets.Worksheet("Result");
+        int _positionRowTo;
+        IXLWorksheet _sheetTo;        
 
         public void CreateBook(string date)
         {
-            FileStream FromRead = new FileStream("res\\test.xls", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            string tempFile=Path.GetTempFileName();
+            FileStream fromRead = new FileStream(tempFile, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
             int positionRowFrom = 15;
-            IWorkbook bookFrom = new HSSFWorkbook(FromRead);
+            IWorkbook bookFrom = new HSSFWorkbook(fromRead);
             ISheet sheetFrom = bookFrom.GetSheetAt(0);
             IRow rowFrom = sheetFrom.GetRow(positionRowFrom);
             ICell cellFrom = rowFrom.GetCell(1);
             IXLRange rngTable;
-
+            _sheetTo.Cell(4, 3).Value = "Период выгрузки " + calFrom.SelectionStart.ToString("yyyy/MM/dd") + " - " + calTo.SelectionStart.ToString("yyyy/MM/dd");
             do
             {
-                sheetTo.Cell(positionRowTo, (2)).Value = date;
+                _sheetTo.Cell(_positionRowTo, (2)).Value = date;
                 for (int i = 1, j = 2; i <= 5; i++)
                 {
                     if (j != 5)
-                        sheetTo.Cell(positionRowTo, (j++ + 1)).Value = cellFrom.StringCellValue;
+                        _sheetTo.Cell(_positionRowTo, (j++ + 1)).Value = cellFrom.StringCellValue;
                     else
                     {
                         j++;
                         i--;
                     }
 
-                    if (sheetTo.Cell(positionRowTo, (8)).GetString() != "" && sheetTo.Cell(positionRowTo, (7)).GetString() != "" && sheetTo.Cell(positionRowTo, (8)).GetString() != "-" && sheetTo.Cell(positionRowTo, (7)).GetString() != "-")
-                        sheetTo.Cell(positionRowTo, (6)).Value = double.Parse(sheetTo.Cell(positionRowTo, (8)).GetString()) / double.Parse(sheetTo.Cell(positionRowTo, (7)).GetString());
+                    if (_sheetTo.Cell(_positionRowTo, (8)).GetString() != "" && _sheetTo.Cell(_positionRowTo, (7)).GetString() != "" && _sheetTo.Cell(_positionRowTo, (8)).GetString() != "-" && _sheetTo.Cell(_positionRowTo, (7)).GetString() != "-")
+                        _sheetTo.Cell(_positionRowTo, (6)).Value = double.Parse(_sheetTo.Cell(_positionRowTo, (8)).GetString()) / double.Parse(_sheetTo.Cell(_positionRowTo, (7)).GetString());
                     cellFrom = rowFrom.GetCell(i + 1);
                 }
-                rngTable = sheetTo.Range("B9:H" + positionRowTo);
+                
+                rngTable = _sheetTo.Range("B9:H" + _positionRowTo);
                 rngTable.Style.Border.RightBorder = XLBorderStyleValues.Thin;
                 rngTable.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
                 rngTable.Style.Border.TopBorder = XLBorderStyleValues.Thin;
                 rngTable.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
-                positionRowTo++;
+                _positionRowTo++;
                 positionRowFrom++;
                 rowFrom = sheetFrom.GetRow(positionRowFrom);
                 cellFrom = rowFrom.GetCell(1);
-            } while (cellFrom.StringCellValue != "Итого:");
-            FromRead.Close();
-            File.Delete("res\\test.xls");
+            } while (cellFrom.StringCellValue != "Итого:");            
+            fromRead.Close();
+            File.Delete(tempFile);
         }
 
         public formMain()
         {
             InitializeComponent();
         }
-
-        public void DowloadFile(string filename)
-        {
-            try { }
-            catch { }
-        }
+      
         private void btCreat_Click(object sender, EventArgs e)
         {
             if ((SFDCreatBook.ShowDialog() == DialogResult.Cancel))
                 return;
-            positionRowTo = 9;
+            XLWorkbook bookTo = new XLWorkbook("res\\maket.xlsx");
+            _sheetTo = bookTo.Worksheets.Worksheet("Result");
+            List<DateTime> missDays = new List<DateTime>();
+            _positionRowTo = 9;
             string myStringWebResource = null;
             var curDate = calFrom.SelectionStart;
             WebClient myWebClient = new WebClient();
-
+            
             while (calTo.SelectionStart >= curDate)
             {
                 try
@@ -95,22 +93,27 @@ namespace Trading_results
                     myStringWebResource = "https://spimex.com/upload/reports/oil_xls/oil_xls_" + curDate.ToString("yyyyMMdd") + "162000.xls";
                     myWebClient.DownloadFile(myStringWebResource, "res\\test.xls");
                     CreateBook(curDate.ToString("yyyy/MM/dd"));
-                    curDate = curDate.AddDays(1);
                 }
                 catch
+                {
+                    missDays.Add(curDate);
+                }
+                finally
                 {
                     curDate = curDate.AddDays(1);
                 }
             }
            
             bookTo.SaveAs(SFDCreatBook.FileName);
-        }
-        private void Checking_dates(DateTime calFrom, DateTime calTo)
-        {
-            if (calFrom <= calTo)
-                btCreat.Visible = true;
+
+            if (missDays.Count != 0)
+                MessageBox.Show("За эти дни нету информации:\n" + string.Join("\n", missDays.Select(i => i.ToString("yyyy/MM/dd")).ToArray()));
             else
-                btCreat.Visible = false;
+                MessageBox.Show("Выполнено!");
+        }
+        private void Checking_dates(DateTime from, DateTime to)
+        {
+            btCreat.Visible = from <= to;
         }
 
         private void Form1_Load(object sender, EventArgs e)
